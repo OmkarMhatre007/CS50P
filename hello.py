c1707a7bbcafec1725bfc91b17a3c0c431fb0b0b
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-01-12 20:55:33 +0530

    Updated cs50

--- hello.py
+++ hello.py
@@ -8,12 +8,50 @@
 
 ##print("Hello " + name + ", welcome to the future")
 
-def hello(to="world"):
-    print("hello,", to)
+#def hello(to="world"):
+#    print("hello,", to)
 
 
-hello()  # prints "hello, world"
-name = input("Enter your name: ")
-hello(name)
+#hello()  # prints "hello, world"
+#name = input("Enter your name: ")
+#hello(name) 
 
-# Rewatch the last 10 mins of cs50P lecture 1 for def function syntax
+# def win(to="aaa"):
+#     print("You win!", to)
+
+# name = input("Enter your name: ")
+# win(name)
+
+# Functions
+# def main():
+#     print("hello, world")
+#     print("This is CS50P")
+
+# main()
+
+#variables
+# def get_guess():
+#     guess = input("Enter your guess: ")
+#     return guess
+
+# def main():
+#     guess = get_guess()
+#     if guess == "70":
+#         print("Correct!")
+#     else:
+#         print("Better luck next time!")
+    
+# main()
+
+#return values
+# def meet(input):
+#     if "hello" in input:
+#         return "Hello, world"
+#     else:
+#         return "I don't understand"
+
+
+# meeting = meet("hello, system")
+# print(meeting)
+
+#side effects
