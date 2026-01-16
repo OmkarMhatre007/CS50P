681c8728e58c2389472a2470f85c3f8df09785a2
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-01-16 20:49:23 +0530

    started cs50p with free certificate course

--- conditional.py
+++ conditional.py
@@ -54,13 +54,50 @@
 #     print("The number is odd.")
 
 # match
-name = input("Enter your name: ")
+# name = input("Enter your name: ")
 
-match name:
-    case "Harry" | "Hermione" | "Ron":
-        print("Griffindor")
-    case "Draco" | "Crabbe" | "Goyle":
-        print("Slytherin")
-    case _:
-        print("Muggle")
+# match name:
+#     case "Harry" | "Hermione" | "Ron":
+#         print("Griffindor")
+#     case "Draco" | "Crabbe" | "Goyle":
+#         print("Slytherin")
+#     case _:
+#         print("Muggle")
 
+#recommendations
+# def main():
+#     difficulty = input("Enter Difficulty of Game (Easy/Medium/Hard): ")
+#     players = input("Single Player or MultiPlayers: ")
+
+#     if difficulty == "Easy" and players == "Single Player":
+#         recommend("Subway Surfers")
+#     elif difficulty == "Medium" and players == "Single Player":
+#         recommend("Shadow Fight 2")
+#     elif difficulty == "Hard" and players == "Single Player":
+#         recommend("Black Myth Wukong")
+#     elif difficulty == "Easy" and players == "MultiPlayers":
+#         recommend("Among Us")
+#     elif difficulty == "Medium" and players == "MultiPlayers":
+#         recommend("Fortnite")
+#     elif difficulty == "Hard" and players == "MultiPlayers":
+#         recommend("Call of Duty: Black Ops & Valorant")
+#     else:
+#         print("No recommendations available")
+    
+# def recommend(game):
+#     print("You might enjoy playing this game: " + game)
+    
+# main()
+
+#password
+password = input("Enter password: ")
+special_symbols = '@#$%^&'
+
+symbols = any(char in special_symbols for char in password)
+
+if password == "admin@123":
+    print("Try strong password")
+elif len(password) >= 8 and symbols == True:
+    print("Password is saved successfully")
+else:
+    print("Invalid Password")
