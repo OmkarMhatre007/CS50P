7bc648ed2387c01e0f55cfdcfeae94df83a0cc3d
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-01-19 23:35:44 +0530

    cs50p week1 completed

---
+++ week1/bank.py
@@ -0,0 +1,10 @@
+greeting = input("Greeting:")
+answer = greeting.strip().lower()
+
+if answer.startswith("hello"):
+    print("$0")
+elif answer.startswith("h"):
+    print("$20")
+else:
+    print("$100")
+
