006b6ba5235e0ad9cd11d0ba01d66ae79fcd0d4b
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-01-20 23:08:59 +0530

    Loops started

---
+++ week2/loops.py
@@ -0,0 +1,25 @@
+#while
+# i = 3 
+# while i != 0:
+#     print("meow")
+#     i = i - 1
+# or  i += 1
+
+#for
+# for i in [0, 1, 2]:
+#     print("meow")
+
+# for i in range(3):
+#     print("meow")
+# print("meow\n" * 3, end="")
+
+# while & for
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("meow")
+
+
