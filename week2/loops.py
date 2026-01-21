ceb0205cdefc4ac5884e3589466d06a71f824efe
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-01-21 23:02:16 +0530

    Update file

--- week2/loops.py
+++ week2/loops.py
@@ -14,12 +14,29 @@
 # print("meow\n" * 3, end="")
 
 # while & for
-while True:
-    n = int(input("What's n? "))
-    if n > 0:
-        break
+# while True:
+#     n = int(input("What's n? "))
+#     if n > 0:
+#         break
 
-for _ in range(n):
-    print("meow")
+# for _ in range(n):
+#     print("meow") 
 
+# def main():
+#     number = get_number()
+#     meow(number)
 
+# def get_number():
+#     while True:
+#         n = int(input("What's n?"))
+#         if n > 0:
+#             break
+#     return n
+
+# def meow(number):
+#     for _ in range(number):
+#         print("meow")
+
+# main()
+
+#hogwarts
