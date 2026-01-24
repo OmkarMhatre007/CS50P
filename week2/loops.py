5d7a460ebb12e2b24e062f2f8317905230fc226f
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-01-24 23:34:51 +0530

    Lecture 2 completed

--- week2/loops.py
+++ week2/loops.py
@@ -83,10 +83,20 @@
 
 # main()
 
+# def main():
+#     print_row(4)
+
+# def print_row(width):
+#     print("?" * width)
+# main()
+
 def main():
-    print_row(4)
+    print_square(3)
 
-def print_row(width):
-    print("?" * width)
+def print_square(size):
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
+
 main()
-
