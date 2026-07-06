656d8c2f154d3b512bcda4fc022e0bf598c185d5
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-07-06 12:55:28 +0530

    cs50p progress

--- week6/shorts.py
+++ week6/shorts.py
@@ -12,21 +12,47 @@
 
 
 ##Reading and Writing CSVs
-import csv
-import numpy as np
-from PIL import Image
+# import csv
+# import numpy as np
+# from PIL import Image
 
+# def main():
+#     with open("views.csv", "r") as file, open("analysis.csv", "w", newline="") as analysis:
+#         reader =csv.DictReader(file)
+#         writer = csv.DictWriter(analysis, fieldnames=["Id", "Title", "Brightness"])
+#         writer.writeheader()
+    
+#         for row in reader:
+#             row["Brightness"] = round(calculate_brightness(f"{row['Id']}.jpeg"), 2)
+#             writer.writerow(row)
+#             # brightness = calculate_brightness(f"{row['Id']}.jpeg")
+#             # writer.writerow(
+#             #     {
+#             #         "Id": row["Id"],
+#             #         "Title": row["Title"],
+#             #         "Brightness": round(brightness, 2)
+#             #     })
+
+#             # print(row)
+
+# def calculate_brightness(filename):
+#     with Image.open(filename) as image:
+#         brightness = np.mean(np.array(image.convert("L"))) / 255
+#     return brightness
+
+# main()
+
+
+##Reading and Writing Files
 def main():
-    with open("views.csv", "r") as file:
-        reader =csv.DictReader(file)
-        for row in reader:
-            print(row)
+    with open("HP.txt", "r") as f:
+        # contents = f.read()
+        contents = f.readlines()
 
-def calculate_brightness(filename):
-    with Image.open(filename) as image:
-        brightness = np.mean(np.array(image.convert("L"))) / 255
-    return brightness
+    chapter1 = contents[3:119]
+    with open("chapter1.txt", "w") as f:
+        f.writelines(chapter1)
+    # print(chapter1[0])
+    # print(contents[0])
 
 main()
-
-# Lecture watched till def main() completion and run 7-8 mins only out of 20 mins.
