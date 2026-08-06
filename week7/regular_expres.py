8ca8ab81a4948fb308a8fe71cc10dadce889b303
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-08-06 23:42:46 +0530

    cs50p week7 progress

--- week7/regular_expres.py
+++ week7/regular_expres.py
@@ -4,7 +4,7 @@ import re
 email = input("Enter your email address: ").strip()
 
 # re.search(pattern, string, flags=0)
-if re.search(r".+@.+\.com", email):
+if re.search(r"^\w+@(\w+\.)?\w+\.(com|edu)$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
