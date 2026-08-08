3b35995c6a0853d6c9a7c8b86e1b7e3cd827ac2d
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-08-08 22:26:42 +0530

    cs50p week7 progress

--- week7/regular_expres.py
+++ week7/regular_expres.py
@@ -1,10 +1,31 @@
 # Validate
+# import re
+
+# email = input("Enter your email address: ").strip()
+
+# # re.search(pattern, string, flags=0)
+# if re.search(r"^[a-z0-9_\.]+@(\w+\.)?\w+\.(com|edu)$", email, re.IGNORECASE):
+#     print("Valid")
+# else:
+#     print("Invalid")
+
+# Format
+# import re
+
+# name = input("Enter your name: ").strip()
+# if matches := re.search(r"^(.+), *(.+)$", name):
+#     name = matches.group(2) + " " + matches.group(1)
+# print(f"Hello, {name}!")
+
+# twitter
 import re
 
-email = input("Enter your email address: ").strip()
+url = input("URL: ").strip()
 
-# re.search(pattern, string, flags=0)
-if re.search(r"^\w+@(\w+\.)?\w+\.(com|edu)$", email, re.IGNORECASE):
-    print("Valid")
-else:
-    print("Invalid")
+matches = re.search(r"^https?://(www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE)
+# if matches = re.search(r"^https?://(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
+if matches:
+    print(f"Username: {matches.group(2)}")
+
+# username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
+# print(f"Username: {username}")
