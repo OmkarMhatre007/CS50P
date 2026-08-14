46562a6a83ee61caf28ea566583ce89ddd4ddf1d
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-08-14 23:55:51 +0530

    cs50p progress

--- week7/watch.py
+++ week7/watch.py
@@ -1,5 +1,4 @@
 import re
-import sys
 
 
 def main():
@@ -7,10 +6,10 @@ def main():
 
 
 def parse(s):
-    ...
-
-
-...
+    matches = re.search(
+        r"(^<iframe .+)http(s)?://(www\.)?youtube\.com/embed/([a-z0-9A-Z?=_]+)", s, re.IGNORECASE)
+    if matches:
+        return f"https://youtu.be/{matches.group(4)}"
 
 
 if __name__ == "__main__":
