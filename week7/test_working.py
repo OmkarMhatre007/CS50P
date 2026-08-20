00c1ec78f1d340c8eceb092a95a42631fabc53d6
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-08-20 21:42:08 +0530

    cs50p progress

--- week7/test_working.py
+++ week7/test_working.py
@@ -1,1 +1,28 @@
+import pytest
 from working import convert
+
+def test_to():
+    with pytest.raises(ValueError):
+        convert("9 AM - 5 PM")
+    with pytest.raises(ValueError):
+        convert("09:00 AM - 17:00 PM")
+
+def test_minutes():
+    with pytest.raises(ValueError):
+        convert("9:60 AM to 5:60 PM")
+    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+    assert convert("9:26 AM to 2:43 PM") == "09:26 to 14:43"
+
+def test_hours():
+    with pytest.raises(ValueError):
+        convert("13:20 AM to 8 PM")
+    with pytest.raises(ValueError):
+        convert("8 AM to 14:40 PM")
+    assert convert("8 AM to 3 PM") == "08:00 to 15:00"
+    assert convert("6 PM to 1 AM") == "18:00 to 01:00"
+    assert convert("12 PM to 1 PM") == "12:00 to 13:00"
+
+def test_zeros():
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("12 AM to 6 AM") == "00:00 to 06:00"
+    assert convert("11 PM to 12:15 AM") == "23:00 to 00:15"
--- week7/working.py
+++ week7/working.py
@@ -5,60 +5,64 @@ def main():
 
 def convert(s):
     check = re.search(r"^(\d{1,2})(\:)?(\d{1,2})? (AM|PM) to (\d{1,2})(\:)?(\d{1,2})? (AM|PM)$", s, re.IGNORECASE)
-    hours1 = int(check.group(1)) 
-    minutes1 = int(check.group(3) or 0) 
-    am_pm1 = (check.group(4))
-    hours2 = int(check.group(5)) 
-    minutes2 = int(check.group(7) or 0) 
-    am_pm2 = (check.group(8))
     if check:
-        if hours1 == None or hours2 == None:
-            raise ValueError
+        hours1 = int(check.group(1))
+        minutes1 = int(check.group(3) or 0)
+        am_pm1 = (check.group(4))
+        hours2 = int(check.group(5))
+        minutes2 = int(check.group(7) or 0)
+        am_pm2 = (check.group(8))
         if hours1 > 12 or hours2 > 12:
             raise ValueError
         else:
             return f"{left(hours1,minutes1,am_pm1)} to {right(hours2,minutes2,am_pm2)}"
+    if not check:
+        raise ValueError
 
 def left(hours1,minutes1,am_pm1):
+    am_pm1 = am_pm1.upper()
     if minutes1 > 59:
         raise ValueError
     elif am_pm1 == "AM":
         if hours1 == 12:
-            new1 = hours1.replace(12, 00)
-            return f"{new1}:{minutes1}"
-        elif minutes1 == " ":
-            return f"{hours1}:00"
+            hours1 = 0
+            return f"{hours1:02d}:{minutes1:02d}"
+        elif minutes1 == 0:
+            return f"{hours1:02d}:{minutes1:02d}"
         else:
-            return f"{hours1}:{minutes1}"
+            return f"{hours1:02d}:{minutes1:02d}"
     elif am_pm1 == "PM":
+        if hours1 == 12:
+            hours1 = 12
+            return f"{hours1:02d}:{minutes1:02d}"
         if hours1 < 12:
-            new2 = hours1 + 12
-            if minutes1 == " ":
-                return f"{new2}:00"
-            return f"{new2}:{minutes1}"
-        if 12 < hours1 < 24:
-            return f"{hours1}:{minutes1}"
-    
+            new1 = hours1 + 12
+            if minutes1 == 0:
+                return f"{new1}:{minutes1:02d}"
+            return f"{new1}:{minutes1:02d}"
+
 def right(hours2,minutes2,am_pm2):
+    am_pm2 = am_pm2.upper()
     if minutes2 > 59:
-            raise ValueError
+        raise ValueError
     elif am_pm2 == "AM":
         if hours2 == 12:
-            new3 = hours2.replace(12, 00)
-            return f"{new3}:{minutes2}"
-        elif minutes2 == " ":
-            return f"{hours2}:00"
+            hours2 = 0
+            return f"{hours2:02d}:{minutes2:02d}"
+        elif minutes2 == 0:
+            return f"{hours2:02d}:{minutes2:02d}"
         else:
-            return f"{hours2}:{minutes2}"
+            return f"{hours2:02d}:{minutes2:02d}"
     elif am_pm2 == "PM":
+        if hours2 == 12:
+            hours2 = 12
+            return f"{hours2:02d}:{minutes2:02d}"
         if hours2 < 12:
-            new4 = hours2 + 12
-            if minutes2 == " ":
-                return f"{new4}:00"
-            return f"{new4}:{minutes2}"
-        if 12 < hours2 < 24:
-            return f"{hours2}:{minutes2}"
-                
+            new2 = hours2 + 12
+            if minutes2 == 0:
+                return f"{new2}:{minutes2:02d}"
+            return f"{new2}:{minutes2:02d}"
+
 if __name__ == "__main__":
     main()
 
@@ -67,4 +71,3 @@ if __name__ == "__main__":
 #2.AM and PM should input capital
 #3.ValueError if input format is wrong
 #4.AM or PM anyone can we 1st in sentence input
-#5.
