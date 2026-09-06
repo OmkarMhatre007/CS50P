4b4e8f37949a9f089d3215758eb9921f0575bf6b
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-09-06 23:46:24 +0530

    cs50p week7 completed

--- week7/response.py
+++ week7/response.py
@@ -1,11 +1,12 @@
-import re
 import validators
 
 def main():
     print(check(input("What's your email address? ")))
 
-def check():
-    ...
-
+def check(s):
+    if validators.email(s):
+        return "Valid"
+    else:
+        return "Invalid"
 if __name__ == "__main__":
     main()
