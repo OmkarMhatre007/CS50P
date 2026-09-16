8b1c17cad10949f248adf4a5aff38c60f57e6bc7
OmkarMhatre007 <197093808+OmkarMhatre007@user.noreply.github.com>
2026-09-16 21:06:32 +0530

    cs50p week8 started

---
+++ week8/oop.py
@@ -0,0 +1,35 @@
+##student 
+# def main():
+#     student = get_student()
+#     if student["name"] == "Padma":
+#         student["house"] = "Ravenclaw"
+#     print(f"{student['name']} from {student['house']}")
+
+# def get_student():
+#     student = {}
+#     student["name"] = input("Name: ")
+#     student["house"] = input("House: ")
+#     return student 
+
+# if __name__ == "__main__":
+#     main()
+
+##Classes
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house)
+    return student
+
+
+if __name__ == "__main__":
+    main()
